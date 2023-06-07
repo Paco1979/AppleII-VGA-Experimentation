19cf2ac1c97e4ad399a704802efe97a7909e45b3
fgutier2 <fgutier2@jaguarlandrover.com>
2023-06-07 22:32:48 +0100

    Functioning Double High Res (King Quest 1 WORKS!)

--- pico/render_lores.c
+++ pico/render_lores.c
@@ -4,6 +4,12 @@
 #include "vga.h"
 
 static void render_lores_line(uint line);
+static void drender_lores_line(uint line);
+
+uint8_t dgr_dot_pattern[32] = {
+    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x08, 0x19, 0x2A, 0x3B, 0x4C, 0x5D, 0x6E, 0x7F,
+    0x00, 0x44, 0x08, 0x4C, 0x11, 0x55, 0x19, 0x5D, 0x22, 0x66, 0x2A, 0x6E, 0x33, 0x77, 0x3B, 0x7F,
+};
 
 void __time_critical_func(render_lores)()
 {
@@ -20,7 +26,14 @@ void __time_critical_func(render_lores)()
 
   for (uint line = 0; line < 24; line++)
   {
-    render_lores_line(line);
+    if (soft_dhires)
+    {
+      drender_lores_line(line);
+    }
+    else
+    {
+      render_lores_line(line);
+    }
   }
 }
 
@@ -39,9 +52,16 @@ void __time_critical_func(render_mixed_lores)()
 
   for (uint line = 0; line < 20; line++)
   {
-    render_lores_line(line);
+    if (soft_dhires)
+    {
+      drender_lores_line(line);
+    }
+    else
+    {
+      render_lores_line(line);
+    }
   }
-  if (soft_80col)
+  if (soft_80col || soft_dhires)
   {
     for (uint line = 20; line < 24; line++)
     {
@@ -64,8 +84,8 @@ static void __time_critical_func(render_lores_line)(uint line)
   struct vga_scanline* sl2 = vga_prepare_scanline();
   uint sl_pos = 0;
 
-  const uint8_t* page = ((soft_switches & SOFTSW_PAGE_2) && !soft_80store) ? text_memory + 1024 : text_memory;
-  const uint8_t* line_buf = page + ((line & 0x7) << 7) + (((line >> 3) & 0x3) * 40);
+  const uint8_t* page = (const uint8_t*)(((soft_switches & SOFTSW_PAGE_2) && !soft_80store) ? text_p2 : text_p1);
+  const uint8_t* line_buf = (const uint8_t*)(page + ((line & 0x7) << 7) + (((line >> 3) & 0x3) * 40));
 
   // Pad 40 pixels on the left to center horizontally
   sl1->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
@@ -97,6 +117,111 @@ static void __time_critical_func(render_lores_line)(uint line)
   sl1->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
   sl2->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
   sl_pos++;
+  sl1->length = sl_pos;
+  sl1->repeat_count = 7;
+  vga_submit_scanline(sl1);
+
+  sl2->length = sl_pos;
+  sl2->repeat_count = 7;
+  vga_submit_scanline(sl2);
+}
+
+static void __time_critical_func(drender_lores_line)(uint line)
+{
+  // Construct two scanlines for the two different colored cells at the same time
+  struct vga_scanline* sl1 = vga_prepare_scanline();
+  struct vga_scanline* sl2 = vga_prepare_scanline();
+  uint sl_pos = 0;
+  uint i, j;
+  uint32_t color1, color2;
+  uint_fast8_t dotc = 0;
+  uint32_t pixeldata;
+
+  const uint8_t* line_bufa = (const uint8_t*)((((soft_switches & SOFTSW_PAGE_2) && !soft_80store) ? text_p2 : text_p1) +
+                                              ((line & 0x7) << 7) + (((line >> 3) & 0x3) * 40));
+
+  const uint8_t* line_bufb = (const uint8_t*)((((soft_switches & SOFTSW_PAGE_2) && !soft_80store) ? text_p4 : text_p3) +
+                                              ((line & 0x7) << 7) + (((line >> 3) & 0x3) * 40));
+
+  // Pad 40 pixels on the left to center horizontally
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl_pos++;
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl_pos++;
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
+  sl_pos++;
+
+  i = 0;
+  color1 = 0;
+  color2 = 0;
+
+  while (i < 40)
+  {
+    while ((dotc <= 14) && (i < 40))
+    {
+      color1 |= dgr_dot_pattern[((i & 1) << 4) | (line_bufb[i] & 0xf)] << dotc;
+      color2 |= dgr_dot_pattern[((i & 1) << 4) | ((line_bufb[i] >> 4) & 0xf)] << dotc;
+      dotc += 7;
+      color1 |= dgr_dot_pattern[((i & 1) << 4) | (line_bufa[i] & 0xf)] << dotc;
+      color2 |= dgr_dot_pattern[((i & 1) << 4) | ((line_bufa[i] >> 4) & 0xf)] << dotc;
+      dotc += 7;
+      i++;
+    }
+
+    // Consume pixels
+    while ((dotc >= 8) || ((dotc > 0) && (i == 40)))
+    {
+      color1 &= 0xfffffffe;
+      color1 |= (color1 >> 4) & 1;
+      pixeldata = dhgr_palette[color1 & 0xf];
+      color1 &= 0xfffffffc;
+      color1 |= (color1 >> 4) & 3;
+      pixeldata |= dhgr_palette[color1 & 0xf] << 16;
+      sl1->data[sl_pos] = pixeldata;
+
+      color2 &= 0xfffffffe;
+      color2 |= (color2 >> 4) & 1;
+      pixeldata = dhgr_palette[color2 & 0xf];
+      color2 &= 0xfffffffc;
+      color2 |= (color2 >> 4) & 3;
+      pixeldata |= dhgr_palette[color2 & 0xf] << 16;
+      sl2->data[sl_pos] = pixeldata;
+
+      sl_pos++;
+
+      color1 &= 0xfffffff8;
+      color1 |= (color1 >> 4) & 7;
+      pixeldata = dhgr_palette[color1 & 0xf];
+      color1 >>= 4;
+      pixeldata |= dhgr_palette[color1 & 0xf] << 16;
+      sl1->data[sl_pos] = pixeldata;
+
+      color2 &= 0xfffffff8;
+      color2 |= (color2 >> 4) & 7;
+      pixeldata = dhgr_palette[color2 & 0xf];
+      color2 >>= 4;
+      pixeldata |= dhgr_palette[color2 & 0xf] << 16;
+      sl2->data[sl_pos] = pixeldata;
+
+      sl_pos++;
+      dotc -= 4;
+    }
+  }
+
+  // Pad 40 pixels on the right to center horizontally
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl_pos++;
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_7) | ((0 | THEN_EXTEND_7) << 16);  // 16 pixels per word
+  sl_pos++;
+  sl1->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
+  sl2->data[sl_pos] = (0 | THEN_EXTEND_3) | ((0 | THEN_EXTEND_3) << 16);  // 8 pixels per word
+  sl_pos++;
+
   sl1->length = sl_pos;
   sl1->repeat_count = 7;
   vga_submit_scanline(sl1);
